9e0349b39023cbb4cd087faebca323f2004c0cd2
Yevheniia Naturkach <naturkach.yevheniia@student.uzhnu.edu.ua>
2026-04-29 12:35:47 +0300

    [TEMPLATE]: initial chat (fix)

--- apps/client/src/components/ChatList.tsx
+++ apps/client/src/components/ChatList.tsx
@@ -1,12 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { deleteChat, fetchChats } from '../services/chats';
+import { deleteChat } from '../services/chats';
 import { useAuthStore } from '../stores/auth';
 import { setLastChatId } from '../utils/lastOpenChatId';
 import { CreateChat } from './CreateChat';
 import ChatMenu from './ContextMenu';
 import { cn } from '../lib/utils';
 import { ContextMenuTrigger, ContextMenu } from './ui/context-menu';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import type { Chat } from '../api/types';
 import { QueryKeys } from '../lib/queries';
 import { useIntersectionObserver } from '../hooks/useIntersectionObserver';
@@ -59,10 +59,6 @@ const ChatList = ({ currentChatId, setCurrentChatId }: ChatListProps) => {
   const member = (chat: Chat) => {
     return chat.members.find((m) => m.userId !== user?.id);
   };
-
-  useEffect(() => {
-    fetchChats({ page: 1 });
-  }, [query.data]);
 
   const chats = query.data?.pages.flat() || [];
 
